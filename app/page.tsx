913aba5f096962f780be09c5533970777a04f23f
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-11 22:29:02 +0100

    add style guide

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1 className='text-primary font-aeonik text-4xl font-black'>Hour of Solution Website</h1>
-      <h1 className='text-primary font-aeonik text-4xl font-bold'>Hour of Solution Website</h1>
-      <h1 className='text-primary font-aeonik text-4xl font-medium'>Hour of Solution Website</h1>
-
-      <div className='my-10' />
-
-      <h1 className='text-primary font-general-sans text-4xl font-normal'>Hour of Solution Website</h1>
-      <h1 className='text-primary font-general-sans text-4xl font-medium'>Hour of Solution Website</h1>
-      <h1 className='text-primary font-general-sans text-4xl font-semibold'>Hour of Solution Website</h1>
+      <h1 className='text-primary font-aeonik text-4xl font-black mt-5 mb-10'>Under Construction</h1>
     </main>
   )
 }
---
+++ app/style-guide/page.tsx
@@ -0,0 +1,58 @@
+export default function Page() {
+  return (
+    <div>
+      <h1 className="text-primary font-aeonik text-4xl font-black mt-5 mb-10">
+        Style Guide
+      </h1>
+
+      <h1 className="text-primary font-aeonik text-4xl font-black">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-aeonik text-4xl font-bold">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-aeonik text-4xl font-medium">
+        Hour of Solution Website
+      </h1>
+
+      <div className="my-10" />
+
+      <h1 className="text-primary font-general-sans text-4xl font-normal">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-general-sans text-4xl font-medium">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-general-sans text-4xl font-semibold">
+        Hour of Solution Website
+      </h1>
+
+      <div className="my-10" />
+
+      <p className="text-smallest text-primary font-aeonik">
+        I am here testing the shit - Smallest Text
+      </p>
+      <p className="text-detail text-primary font-aeonik">
+        I am here testing the shit - Detail Text
+      </p>
+      <p className="text-body text-primary font-aeonik">
+        I am here testing the shit - Body Text
+      </p>
+      <button className="text-button text-white bg-primary font-aeonik px-8 py-2 rounded-3xl">
+        I am here testing the shit - Button Text
+      </button>
+      <p className="text-heading-4 text-primary font-aeonik">
+        I am here testing the shit - Heading 4 Text
+      </p>
+      <p className="text-heading-3 text-primary font-aeonik">
+        I am here testing the shit - Heading 2 Text
+      </p>
+      <p className="text-heading-2 text-primary font-aeonik">
+        I am here testing the shit - Heading 2 Text
+      </p>
+      <p className="text-heading-1 text-primary font-aeonik">
+        I am here testing the shit - Heading 1 Text
+      </p>
+    </div>
+  );
+}
