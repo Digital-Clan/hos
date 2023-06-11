e0a7c2abd9dfbfd0be2c0125cc71ea7e9d0d7010
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-11 22:33:01 +0100

    updated

--- app/style-guide/page.tsx
+++ app/style-guide/page.tsx
@@ -1,58 +1,60 @@
 export default function Page() {
   return (
-    <div>
-      <h1 className="text-primary font-aeonik text-4xl font-black mt-5 mb-10">
+    <div className="p-5 lg:px-8">
+      <h1 className="text-primary font-aeonik text-4xl font-black mb-10">
         Style Guide
       </h1>
 
-      <h1 className="text-primary font-aeonik text-4xl font-black">
+      <h1 className="text-primary font-aeonik text-4xl font-black mb-2">
         Hour of Solution Website
       </h1>
-      <h1 className="text-primary font-aeonik text-4xl font-bold">
+      <h1 className="text-primary font-aeonik text-4xl font-bold mb-2">
         Hour of Solution Website
       </h1>
-      <h1 className="text-primary font-aeonik text-4xl font-medium">
-        Hour of Solution Website
-      </h1>
-
-      <div className="my-10" />
-
-      <h1 className="text-primary font-general-sans text-4xl font-normal">
-        Hour of Solution Website
-      </h1>
-      <h1 className="text-primary font-general-sans text-4xl font-medium">
-        Hour of Solution Website
-      </h1>
-      <h1 className="text-primary font-general-sans text-4xl font-semibold">
+      <h1 className="text-primary font-aeonik text-4xl font-medium mb-2">
         Hour of Solution Website
       </h1>
 
       <div className="my-10" />
 
-      <p className="text-smallest text-primary font-aeonik">
-        I am here testing the shit - Smallest Text
-      </p>
-      <p className="text-detail text-primary font-aeonik">
-        I am here testing the shit - Detail Text
-      </p>
-      <p className="text-body text-primary font-aeonik">
-        I am here testing the shit - Body Text
-      </p>
-      <button className="text-button text-white bg-primary font-aeonik px-8 py-2 rounded-3xl">
-        I am here testing the shit - Button Text
-      </button>
-      <p className="text-heading-4 text-primary font-aeonik">
-        I am here testing the shit - Heading 4 Text
-      </p>
-      <p className="text-heading-3 text-primary font-aeonik">
-        I am here testing the shit - Heading 2 Text
-      </p>
-      <p className="text-heading-2 text-primary font-aeonik">
-        I am here testing the shit - Heading 2 Text
-      </p>
-      <p className="text-heading-1 text-primary font-aeonik">
-        I am here testing the shit - Heading 1 Text
-      </p>
+      <h1 className="text-primary font-general-sans text-4xl font-normal mb-2">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-general-sans text-4xl font-medium mb-2">
+        Hour of Solution Website
+      </h1>
+      <h1 className="text-primary font-general-sans text-4xl font-semibold mb-2">
+        Hour of Solution Website
+      </h1>
+
+      <div className="my-10" />
+
+      <div className="flex flex-col space-y-3">
+        <p className="text-smallest text-primary font-aeonik">
+          I am here testing the shit - Smallest Text
+        </p>
+        <p className="text-detail text-primary font-aeonik">
+          I am here testing the shit - Detail Text
+        </p>
+        <p className="text-body text-primary font-aeonik">
+          I am here testing the shit - Body Text
+        </p>
+        <button className="text-button text-white bg-primary font-aeonik px-8 py-2 rounded-3xl">
+          I am here testing the shit - Button Text
+        </button>
+        <p className="text-heading-4 text-primary font-aeonik">
+          I am here testing the shit - Heading 4 Text
+        </p>
+        <p className="text-heading-3 text-primary font-aeonik">
+          I am here testing the shit - Heading 2 Text
+        </p>
+        <p className="text-heading-2 text-primary font-aeonik">
+          I am here testing the shit - Heading 2 Text
+        </p>
+        <p className="text-heading-1 text-primary font-aeonik">
+          I am here testing the shit - Heading 1 Text
+        </p>
+      </div>
     </div>
   );
 }
