a8a664cbf4418451e5d0a7cd03962e937194f85d
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-13 20:00:04 +0100

    add icons

---
+++ app/icons/Calendar.jsx
@@ -0,0 +1,93 @@
+export default function CalendarIcon() {
+  return (
+    <svg
+      width="16"
+      height="17"
+      viewBox="0 0 16 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.33333 1.83333V3.83333"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M10.6667 1.83333V3.83333"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M2.33333 6.56H13.6667"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M14 6.16667V11.8333C14 13.8333 13 15.1667 10.6667 15.1667H5.33333C3 15.1667 2 13.8333 2 11.8333V6.16667C2 4.16667 3 2.83334 5.33333 2.83334H10.6667C13 2.83334 14 4.16667 14 6.16667Z"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeMiterlimit="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M10.4631 9.63334H10.4691"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M10.4631 11.6333H10.4691"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M7.99698 9.63334H8.00297"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M7.99698 11.6333H8.00297"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M5.52955 9.63334H5.53553"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M5.52954 11.6333H5.53553"
+        stroke="#656565"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
---
+++ app/icons/Time.jsx
@@ -0,0 +1,27 @@
+export default function TimeIcon() {
+  return (
+    <svg
+      width="16"
+      height="17"
+      viewBox="0 0 16 17"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.6667 8.5C14.6667 12.18 11.68 15.1667 8 15.1667C4.32 15.1667 1.33333 12.18 1.33333 8.5C1.33333 4.82 4.32 1.83333 8 1.83333C11.68 1.83333 14.6667 4.82 14.6667 8.5Z"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        opacity="0.4"
+        d="M10.4733 10.62L8.40666 9.38667C8.04666 9.17333 7.75333 8.66 7.75333 8.24V5.50667"
+        stroke="#656565"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
