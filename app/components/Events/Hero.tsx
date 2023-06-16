4c4feda1e889a79c45a68d5decf2993e7909d845
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-16 12:55:31 +0100

    create events page

---
+++ app/components/Events/Hero.tsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <header>
+      <h1>Events Hero Section</h1>
+    </header>
+  )
+}
---
+++ app/events/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "../components/Events"
+
+export default function Events() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  )
+}
