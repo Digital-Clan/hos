68c85aefae14fa9eae1857dbb8bbbc90a4a97e86
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-17 04:42:07 +0100

    updated

--- app/components/Donate/Hero.tsx
+++ app/components/Donate/Hero.tsx
@@ -1,14 +1,11 @@
-import Link from "next/link";
-import { VideoIcon } from "@/app/icons";
-
 export default function Hero() {
   return (
-    <header className="px-8 py-16 donate-hero text-white lg:pt-20 lg:pb-40">
+    <header className="px-8 py-16 donate-hero text-white sm:py-24 md:py-32 lg:py-36">
       <div className="text-center max-w-[578px] mx-auto">
         <h1 className="text-heading-1 mb-5 font-black md:text-4xl lg:text-5xl lg:mb-8 xl:text-[56px] xl:leading-[65px]">
           Support our cause
         </h1>
-        <p className="font-general-sans text-body-1 font-medium sm:text-base md:text-lg lg:text-2xl lg:leading-[38px]">
+        <p className="font-general-sans para-1x text-body-white sm:text-base md:text-lg lg:text-2xl">
           Lorem ipsum dolor sit amet consectetur adipssising elit, dolor sit
           amet, lorem ipsum dolor sit amet, dolor sit amet, lorem ipsum dolor
           sit amet lorem ipsum.
--- app/components/Media /Hero.tsx
+++ app/components/Media /Hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <header className="px-5 md:px-8 py-20 text-center media-hero text-white md:py-36 lg:text-left">
+    <header className="px-5 md:px-8 py-16 text-center media-hero text-white lg:text-left sm:py-24 md:py-32 lg:py-36">
       <div className="container-block">
         <div className="w-[220px] mx-auto sm:w-2/3 lg:mx-0">
           <h1 className="text-heading-1 mb-3 font-black md:text-4xl lg:text-5xl xl:text-[56px] xl:leading-[65px]">
