90956ceb3f32f5c615d4e6affb0d2762edc55e1e
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-17 05:20:50 +0100

    add about page

---
+++ app/about-us/page.tsx
@@ -0,0 +1,9 @@
+import { Hero } from "../components/About";
+
+export default function About() {
+  return (
+    <main>
+      <Hero />
+    </main>
+  );
+}
---
+++ app/components/About/Hero.tsx
@@ -0,0 +1,13 @@
+export default function Hero() {
+  return (
+    <header className="px-5 md:px-8 py-16 text-center about-hero bg-cover bg-center text-white lg:text-left sm:py-24 md:py-32 lg:py-36">
+      <div className="container-block">
+        <div className="w-[280px] mx-auto sm:w-2/3 lg:mx-0">
+          <h1 className="text-heading-1 font-black md:text-4xl lg:text-5xl xl:text-[56px] xl:leading-[65px]">
+            About us
+          </h1>
+        </div>
+      </div>
+    </header>
+  );
+}
