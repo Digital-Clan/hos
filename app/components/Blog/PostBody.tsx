13c83752a06f871c5ca3d0f43da56ade4af12d02
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-20 10:56:03 +0100

    add post body

---
+++ app/components/Blog/PostBody.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+export default function PostBody() {
+  return (
+    <section className="-mt-32 px-5 pb-10">
+      <div className="">
+        <div className="mb-8 w-full">
+          <img
+            src="https://res.cloudinary.com/dljsalifp/image/upload/v1687250930/hos/blog-img-3_prahuz.png"
+            alt=""
+            className="h-[240px] w-full rounded-[32px] sm:h-[350px] md:h-[240px]"
+          />
+        </div>
+
+        <div className="flex flex-col space-y-8">
+          <div>
+            <p className="font-general-sans text-p1-m font-medium text-body md:text-p1-t lg:text-p1-d">
+              Lorem ipsum dolor sit amet consectetur adipscising elit, dolor sit amet dolor sit amet dolor sit amet
+              consectetur dolor sit amet lorem ipsum, amet consectetur dolor sit amet lorem sit. ipsum sit amet
+              consectetur adipscising. Lorem ipsum dolor sit amet consectetur adipscising elit, dolor sit amet dolor sit
+              amet dolor sit amet consectetur dolor sit amet lorem ipsum, amet. Lorem ipsum dolor sit amet consectetur
+              elit. adipscising elit, dolor sit amet dolor sit amet.
+            </p>
+          </div>
+
+          <div>
+            <h2 className="mb-3 text-h3-m font-bold text-[#4C4C4C] md:text-h3-t lg:text-h3-d">
+              Lorem ipsum dolor sit amet?
+            </h2>
+            <p className="font-general-sans text-p1-m font-medium text-body md:text-p1-t lg:text-p1-d">
+              Lorem ipsum dolor sit amet consectetur adipscising elit, dolor sit amet dolor sit amet dolor sit amet
+              consectetur dolor sit amet lorem ipsum, amet consectetur dolor sit amet lorem sit. ipsum sit amet
+              consectetur adipscising. Lorem ipsum dolor sit amet consectetur adipscising elit, dolor sit amet dolor sit
+              amet dolor sit amet consectetur dolor sit amet lorem ipsum, amet. Lorem ipsum dolor sit amet consectetur
+              elit. adipscising elit, dolor sit amet dolor sit amet.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
