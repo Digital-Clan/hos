3355c6fe8a6415f917a8da59f8d7a9b951f6edeb
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-20 10:27:46 +0100

    add blogposts

--- app/components/Blog/PostsGrid.tsx
+++ app/components/Blog/PostsGrid.tsx
@@ -1,3 +1,42 @@
+/* eslint-disable @next/next/no-img-element */
+import { blogposts } from "@/app/data/blogposts";
+import Link from "next/link";
+import { ArrowIcon } from "@/app/icons";
+
+interface Post {
+  id: number;
+  title: string;
+  slug: string;
+  img: string;
+  date: string;
+  content: string[];
+  excerpt: string;
+}
+
 export default function PostsGrid() {
-  return <section>posts grid</section>;
+  return (
+    <section className="px-5 py-14 lg:py-20">
+      <div className="container-block mx-auto">
+        <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-8 lg:grid-cols-3 xl:gap-x-10 xl:gap-y-12">
+          {blogposts.map(({ id, title, slug, img, excerpt }: Post) => (
+            <Link
+              href={`/blog/${slug}`}
+              className="cursor-pointer rounded-[32px] border border-[#D8D8D8] transition-transform duration-300 ease-in-out hover:-translate-y-2"
+              key={id}
+            >
+              <img src={img} alt={title} className="h-[240px] w-full sm:h-[350px] md:h-[240px]" />
+              <div className="flex flex-col space-y-4 px-5 py-7">
+                <h2 className="text-h4-t font-bold text-black md:text-h3-t">{title}</h2>
+                <p className="text-p1-m text-body md:text-p1-t">{excerpt}</p>
+                <button className="button-link flex items-center space-x-2">
+                  <span className="text-p1-m md:text-p1-t">Read More</span>
+                  <ArrowIcon stroke="#121212" />
+                </button>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
--- app/icons/Arrow.tsx
+++ app/icons/Arrow.tsx
@@ -1,9 +1,9 @@
-export default function ArrowIcon() {
+export default function ArrowIcon({ stroke }: { stroke?: string }) {
   return (
     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
       <path
         d="M14.43 5.92999L20.5 12L14.43 18.07"
-        stroke="#304098"
+        stroke={stroke || "#304098"}
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
@@ -11,7 +11,7 @@ export default function ArrowIcon() {
       />
       <path
         d="M3.5 12H20.33"
-        stroke="#304098"
+        stroke={stroke || "#304098"}
         strokeWidth="1.5"
         strokeMiterlimit="10"
         strokeLinecap="round"
