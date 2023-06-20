f6db047e0d8b436384bf0a0f30209dff73f73615
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-20 10:56:14 +0100

    add post header

---
+++ app/components/Blog/PostHeader.tsx
@@ -0,0 +1,13 @@
+export default function PostHeader() {
+  return (
+    <section className="bg-black px-5 pb-44 pt-16">
+      <div className="mx-auto max-w-[350px] text-center text-white">
+        <span className="rounded-[40px] border-[1.5px] border-[#FEFEFE] px-8 py-2 font-general-sans text-xs-m font-medium uppercase transition-colors duration-200 ease-in-out hover:bg-white hover:text-black">
+          Blog
+        </span>
+        <h1 className="my-5 text-h1-m font-black">Budding Character for Great and mighty things.</h1>
+        <p className="text-h4-m font-bold">May 12, 2023</p>
+      </div>
+    </section>
+  );
+}
