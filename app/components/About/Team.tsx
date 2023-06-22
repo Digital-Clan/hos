350d910266e225f7afb297009fe032d000c9417c
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-22 11:16:11 +0100

    add team section for desktop

--- app/components/About/Team.tsx
+++ app/components/About/Team.tsx
@@ -1,23 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
-function Card() {
+function Card({ name, bio }: { name: string; bio: string }) {
   return (
-    <div className="group h-96 w-80 [perspective:1000px]">
-      <div className="relative h-full w-full rounded-xl shadow-xl transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
-        <div className="absolute inset-0">
-          <img
-            className="h-full w-full rounded-xl object-cover shadow-xl shadow-black/40"
-            src="https://images.unsplash.com/photo-1562583489-bf23ec64651d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80')"
-            alt=""
-          />
-        </div>
-        <div className="absolute inset-0 h-full w-full rounded-xl bg-black/80 px-12 text-center text-slate-200 [backface-visibility:hidden] [transform:rotateY(180deg)]">
-          <div className="flex min-h-full flex-col items-center justify-center">
-            <h1 className="text-3xl font-bold">Jane Doe</h1>
-            <p className="text-lg">Photographer & Art</p>
-            <p className="text-base">Lorem ipsum dolor sit amet consectetur adipisicing.</p>
-            <button className="mt-2 rounded-md bg-neutral-800 px-2 py-1 text-sm hover:bg-neutral-900">Read More</button>
-          </div>
-        </div>
+    <div className="team-card relative overflow-hidden">
+      <img
+        className="h-full w-full object-cover transition-all duration-300 ease-in-out"
+        src="https://res.cloudinary.com/dljsalifp/image/upload/v1687404117/hos/pastor-card-img_spbljd.png"
+        alt=""
+      />
+
+      <div className="member absolute left-0 right-0 top-0 mx-auto flex h-full w-full max-w-[350px] flex-col items-center justify-center space-y-3 bg-transparent text-center text-white transition-all duration-200 ease-in-out">
+        <h3 className="font-general-sans text-h4-d font-semibold">{name}</h3>
+        <p className="font-general-sans text-lg font-medium">{bio}</p>
       </div>
     </div>
   );
@@ -58,12 +51,20 @@ export default function Team() {
   ];
 
   return (
-    <section className="px-5 py-10">
+    <section className="px-5 py-10 md:py-16 lg:py-20">
       <div className="container-block mx-auto">
-        <div className="grid grid-cols-3 gap-5">
-          {team.map((member) => (
-            <Card key={member.id} />
-          ))}
+        <div className="hidden lg:block">
+          <div className="text-center lg:mb-8">
+            <h2 className="mb-1 text-h3-m font-bold md:text-h3-t lg:text-h3-d">Meet Our Team</h2>
+            <p className="mb-2 block font-general-sans text-xs-m font-medium text-secondary md:text-xs-t lg:mb-0 lg:text-xs-d">
+              Committee Members
+            </p>
+          </div>
+          <div className="grid grid-cols-3 gap-5">
+            {team.map((member) => (
+              <Card key={member.id} name={member.name} bio={member.bio} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
