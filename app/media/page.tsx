a1a9c2bfb41b68fcf952d5a506a631e17023c101
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-26 11:04:21 +0100

    updated

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -10,7 +10,7 @@ export default async function Media() {
       <header className="bg-media-m bg-cover bg-center px-5 py-16 text-center text-white sm:py-24 md:px-8 md:py-32 lg:bg-media-d lg:py-36 lg:text-left">
         <div className="container-block">
           <div className="mx-auto w-[220px] sm:w-2/3 lg:mx-0">
-            <h1 className="text mb-3 text-h1-m font-black md:text-p1-t lg:text-p1-d">Media</h1>
+            <h1 className="text mb-3 text-h1-m font-black md:text-h1-t lg:text-h1-d">Media</h1>
             <p className="font-general-sans text-p1-m text-body-white sm:text-base md:text-p1-t lg:text-p1-d">
               Tune in to the latest events at Hour of Solution
             </p>
