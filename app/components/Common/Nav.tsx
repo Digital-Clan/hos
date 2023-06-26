0821a5ac99b0883f260975ad0131af55c09b170a
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-06-26 17:01:17 +0100

    update logo size

--- app/components/Common/Nav.tsx
+++ app/components/Common/Nav.tsx
@@ -38,11 +38,7 @@ export default function Nav() {
       <nav className="relative z-[50] bg-primary p-5 text-white lg:py-6">
         <div className="container-block flex items-center justify-between">
           <Link onClick={() => setIsMenuOpen(false)} href="/" className="text-xl lg:text-3xl">
-            <img
-              className="h-[50px] w-[50px] md:h-[70px] md:w-[70px]"
-              src="/hour-of-solution-logo.jpg"
-              alt="Hour of Solution Logo"
-            />
+            <img className="h-[50px] w-[50px]" src="/hour-of-solution-logo.jpg" alt="Hour of Solution Logo" />
           </Link>
 
           <ul className="hidden lg:flex lg:space-x-12">
