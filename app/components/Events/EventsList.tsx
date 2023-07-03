593a9a0b39c4b3126e9e2463167318ffe168fb57
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-07-03 20:18:58 +0100

    updated

--- app/components/Events/EventsList.tsx
+++ app/components/Events/EventsList.tsx
@@ -1,14 +1,30 @@
 "use client";
+import moment from "moment";
 import EventCard from "../Common/EventCard";
 import { Event } from "@/sanity/lib/types";
 
 type Props = {
-  liveEvents: Event[];
-  upcomingEvents: Event[];
+  events: Event[];
 };
 
-export default function EventsList({ liveEvents, upcomingEvents }: Props) {
-  console.log(upcomingEvents);
+export default function EventsList({ events }: Props) {
+  const filteredUpcomingEvents = events.filter((event) => {
+    const eventDate = moment(event.startTime);
+    const currentDate = moment();
+    const diff = eventDate.diff(currentDate, "seconds");
+    return diff > 0;
+  });
+
+  const filterLiveEvents = events.filter((event) => {
+    const eventStartTime = moment(event.startTime);
+    const eventEndTime = moment(event.endTime);
+    const currentDate = moment();
+
+    const diffStart = eventStartTime.diff(currentDate, "seconds");
+    const diffEnd = eventEndTime.diff(currentDate, "seconds");
+
+    return diffStart < 0 && diffEnd > 0;
+  });
 
   return (
     <>
@@ -19,8 +35,8 @@ export default function EventsList({ liveEvents, upcomingEvents }: Props) {
               Live Events
             </h2>
             <div className="flex flex-col space-y-10">
-              {liveEvents?.length > 0 ? (
-                liveEvents.map((event, index: number) => <EventCard key={index} {...event} />)
+              {filterLiveEvents?.length > 0 ? (
+                filterLiveEvents.map((event, index: number) => <EventCard key={index} {...event} />)
               ) : (
                 <p className="py-5 text-center text-p1-m text-body md:text-p1-t lg:text-p1-d">No live events found</p>
               )}
@@ -32,8 +48,8 @@ export default function EventsList({ liveEvents, upcomingEvents }: Props) {
               Upcoming Events
             </h2>
             <div className="flex flex-col space-y-10">
-              {upcomingEvents?.length > 0 ? (
-                upcomingEvents.map((event, index: number) => <EventCard key={index} {...event} />)
+              {filteredUpcomingEvents?.length > 0 ? (
+                filteredUpcomingEvents.map((event, index: number) => <EventCard key={index} {...event} />)
               ) : (
                 <p className="py-5 text-center text-p1-m text-body md:text-p1-t lg:text-p1-d">
                   No upcoming events found
