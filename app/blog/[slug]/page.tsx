3e9ee4e52a39b964832c93ecba08e0f5f030bc41
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-07-06 00:24:20 +0100

    update title

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,11 +1,33 @@
 import { PostHeader, PostBody, PostShare, PostComments } from "@/app/components/Blog";
 import { getPostBySlug } from "@/sanity/lib/util";
 
+import { Metadata, ResolvingMetadata } from "next";
+
 type Props = {
   params: {
     slug: string;
   };
 };
+
+export async function generateMetadata({ params }: Props, parent?: ResolvingMetadata): Promise<Metadata> {
+  const slug = params.slug;
+  const post = await getPostBySlug(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+    openGraph: {
+      images: [
+        {
+          url: post.coverImage,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+  };
+}
 
 export const revalidate = 30;
 
