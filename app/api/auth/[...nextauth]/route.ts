7b8579d99e2652c0cac039cb4c0bf9dc04783705
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-07-07 12:14:34 +0100

    done 2

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,16 @@
-import NextAuth from "next-auth";
-import type { NextApiRequest, NextApiResponse } from "next";
-import GoogleProvider from "next-auth/providers/google";
+// import NextAuth from "next-auth";
+// import type { NextApiRequest, NextApiResponse } from "next";
+// import GoogleProvider from "next-auth/providers/google";
 
 // const handler = (req: NextApiRequest, res: NextApiResponse) => {
 // export default async function auth(req: NextApiRequest, res: NextApiResponse) {
-const handler = NextAuth({
-  providers: [
-    GoogleProvider({
-      clientId: process.env.NEXT_APP_GOOGLE_ID,
-      clientSecret: process.env.NEXT_APP_GOOGLE_SECRET,
-    }),
-  ],
-});
+// const handler = NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.NEXT_APP_GOOGLE_ID,
+//       clientSecret: process.env.NEXT_APP_GOOGLE_SECRET,
+//     }),
+//   ],
+// });
 
-export { handler as GET, handler as POST };
+// export { handler as GET, handler as POST };
