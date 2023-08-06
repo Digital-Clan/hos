36d9e6ed3bb05188f375557f0d179ccb28b1757a
Abdulsamad Ayoade <ayoadesamad@gmail.com>
2023-08-06 23:53:33 +0100

    update blog images

--- app/components/Blog/PostBody.tsx
+++ app/components/Blog/PostBody.tsx
@@ -12,9 +12,15 @@ export default function PostBody({ image, content }: Props) {
   return (
     <section className="-mt-32 px-5 pb-10 sm:-mt-44">
       <div className="mx-auto max-w-[1037px]">
-        <div className="mb-8 w-full">
-          <img src={image} alt="" className="w-full rounded-[32px] object-cover md:h-[460px] lg:h-[490px]" />
-        </div>
+        <div
+          style={{
+            backgroundImage: `url(${image})`,
+            backgroundSize: "cover",
+            borderRadius: "32px",
+            backgroundPosition: "top center",
+          }}
+          className="mb-8 h-[250px] w-full sm:h-[400px] md:h-[460px] lg:h-[490px]"
+        ></div>
 
         <div className="blogpost text-red-500">
           <PortableText value={content} />
--- app/components/Blog/PostsGrid.tsx
+++ app/components/Blog/PostsGrid.tsx
@@ -11,17 +11,21 @@ export default function PostsGrid({ posts }: Props) {
   return (
     <section className="px-5 py-14 lg:py-20">
       <div className="container-block mx-auto">
-        <div className="grid grid-cols-1 gap-y-10 md:grid-cols-2 md:gap-x-8 lg:grid-cols-3 xl:gap-x-10 xl:gap-y-12">
+        <div className="grid grid-cols-1 gap-y-10 sm:grid-cols-2 sm:gap-x-8 xl:grid-cols-3 xl:gap-x-10 xl:gap-y-12">
           {posts.map(({ title, slug, coverImage, excerpt }, index) => (
             <Link
               href={`/blog/${slug}`}
               className="cursor-pointer overflow-hidden rounded-[32px] border border-[#D8D8D8] transition-transform duration-300 ease-in-out hover:-translate-y-2"
               key={index}
             >
-              <img src={coverImage} alt={title} className="h-[240px] w-full sm:h-[350px] md:h-[240px]" />
-              <div className="flex flex-col space-y-4 px-5 py-7">
-                <h2 className="text-h4-t font-bold text-black md:text-h3-t">{title}</h2>
-                <p className="text-p1-m text-body md:text-p1-t">{excerpt}</p>
+              <div className="relative h-[240px] w-full md:h-[300px] lg:h-[350px]">
+                <img src={coverImage} alt={title} className="h-full w-full object-fill" />
+              </div>
+              <div className="flex flex-col space-y-4 px-5 py-7 xl:h-[350px] xl:justify-between">
+                <div className="flex flex-col space-y-4">
+                  <h2 className="text-h4-t font-bold text-black md:text-h3-t">{title}</h2>
+                  <p className="text-p1-m text-body md:text-p1-t">{excerpt}</p>
+                </div>
                 <button className="button-link flex items-center space-x-2">
                   <span className="text-p1-m md:text-p1-t">Read More</span>
                   <ArrowIcon stroke="#121212" />
